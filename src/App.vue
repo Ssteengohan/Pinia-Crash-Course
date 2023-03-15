4de3b6969169bfc12f24c36e9ab1190cf9007e80
FIRST_GIO LAST-TODOREVIC <EMAIL_2048757@TALNET.NL>
2023-03-15 15:43:02 +0100

    Ep5 - Getters

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,51 @@
 <template>
   <main>
-
     <!-- Heading -->
     <header>
-      <img src="./assets/pinia-logo.svg" alt="pinia-logo">
+      <img src="./assets/pinia-logo.svg" alt="pinia-logo" />
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
 
     <!-- task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
+      <p>Your have {{ taskStore.totalCount }} tasks left to do</p>
       <div v-for="task in taskStore.tasks">
-        <TaskDetails :task="task"/>
+        <TaskDetails :task="task" />
       </div>
     </div>
+    <!-- task list -->
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>Your have {{ taskStore.favCount }} favs left to do</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
   </main>
 </template>
 
 <script>
-import TaskDetails from './components/TaskDetails.vue';
-import { useTaskStore } from './stores/TaskStore';
+import { ref } from 'vue';
+import TaskDetails from "./components/TaskDetails.vue";
+import { useTaskStore } from "./stores/TaskStore";
 
+export default {
+  components: {
+    TaskDetails,
+  },
+  setup() {
+    const taskStore = useTaskStore();
 
-  export default {
-    components: {
-      TaskDetails
-    },
-    setup() {
-      const taskStore = useTaskStore()
-      
-      return { taskStore }
-    }
-  }
+    const filter = ref('all');
+
+    return { taskStore, filter };
+  },
+};
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -1,11 +1,23 @@
-import { defineStore } from "pinia"
+import { defineStore } from "pinia";
 
-
-export const useTaskStore = defineStore('taskStore', {
-    state: () => ({
-        tasks: [
-            {id: 1, title: "buy some milk", isFave: false},
-            {id: 2, title: "play Gloomhaven", isFave: true},
-        ],  
-    })
-})
+export const useTaskStore = defineStore("taskStore", {
+  state: () => ({
+    tasks: [
+      { id: 1, title: "buy some milk", isFave: false },
+      { id: 2, title: "play Gloomhaven", isFave: true },
+    ],
+  }),
+  getters: {
+    favs() {
+      return this.tasks.filter((t) => t.isFave);
+    },
+    favCount() {
+      return this.tasks.reduce((p, c) => {
+        return c.isFave ? p + 1 : p;
+      }, 0);
+    },
+    totalCount: (state) => {
+        return state.tasks.length;
+    }
+  },
+});
