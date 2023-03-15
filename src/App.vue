34c310cb4f42eb58156bfc4cbcaa2bd01d0c87c0
FIRST_GIO LAST-TODOREVIC <EMAIL_2048757@TALNET.NL>
2023-03-15 14:22:08 +0100

    Ep 2 Pinia Crash Course

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+
+    <!-- Heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia-logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
