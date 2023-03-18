36fa5813b68f5fa5d053472554841361dd46661e
FIRST_GIO LAST-TODOREVIC <EMAIL_2048757@TALNET.NL>
2023-03-18 14:22:48 +0100

    Ep9 - Async Actions (part 2)

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -15,28 +15,58 @@ export const useTaskStore = defineStore("taskStore", {
       }, 0);
     },
     totalCount: (state) => {
-        return state.tasks.length;
-    }
+      return state.tasks.length;
+    },
   },
-    actions: {
-      async getTasks() {
-        this.loading = true;
-        const response = await fetch("http://localhost:3000/tasks")
-        const data = await response.json();
-        this.tasks = data;
-        this.loading = false;
-      },
-        addTask(task) {
-            this.tasks.push(task);
-        },
-        deleteTask(id) {
-          this.tasks = this.tasks.filter(t => {
-            return t.id !== id;
-          });
-        },
-        toggleFav(id) {
-          const task = this.tasks.find(t => t.id === id);
-          task.isFav = !task.isFav;
-        }
-    }
+  actions: {
+    async getTasks() {
+      this.loading = true;
+
+      const response = await fetch("http://localhost:3000/tasks");
+      const data = await response.json();
+
+      this.tasks = data;
+      this.loading = false;
+    },
+    async addTask(task) {
+      this.tasks.push(task);
+
+      const response = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.error) {
+        console.log(response.error);
+      }
+    },
+    async deleteTask(id) {
+      this.tasks = this.tasks.filter(t => {
+        return t.id !== id;
+      });
+    
+      const response = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "DELETE",
+      });
+
+      if (response.error) {
+        console.log(response.error);
+      }
+    },
+    async toggleFav(id) {
+      const task = this.tasks.find(t => t.id === id);
+      task.isFav = !task.isFav;
+      
+      const response = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "PATCH",
+        body: JSON.stringify({ isFav: task.isFav }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.error) {
+        console.log(response.error);
+      }
+    },
+  },
 });
